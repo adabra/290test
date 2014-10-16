c071c44206100c1d3ed4222b491ff6c59a6ce049
Audun Brandt <audundb@gmail.com>
2014-10-15 18:42:57 -0700

    added name validation and associated tests for community

--- testapp/test/models/community_test.rb
+++ testapp/test/models/community_test.rb
@@ -6,4 +6,26 @@ class CommunityTest < ActiveSupport::TestCase
 		assert community.invalid?
 		assert community.errors[:name].any?
 	end
+
+	test "create community with valid name" do
+		c = Community.new name: 'Good'
+		assert c.valid?, 'community should be ok'
+
+		c = Community.new name: 'Good but very long community name'
+		assert c.valid?, 'community should be ok'
+	end
+
+	test "fail when community name is too small" do
+		c = Community.new name: 'Bad'
+		assert c.invalid?
+
+		c = Community.new name: '      '
+		assert c.invalid?
+
+		c = Community.new name: nil
+		assert c.invalid?
+
+		c = Community.new name: 1
+		assert c.invalid?
+	end
 end
